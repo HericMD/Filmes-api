d7f6b3a73ff6be0e6b813a08d67b26966de83c3f
HericMD <hericmdms@gmail.com>
2022-10-19 15:12:35 -0300

    19outubro

--- filmes-api/src/views/CategoriasView.vue
+++ filmes-api/src/views/CategoriasView.vue
@@ -17,7 +17,13 @@ export default {
 };
 </script>
 <template>
-  <p v-for="genre of genres" :key="genre.id">
+  <select v-model="genre_id">
+    <option value="teste">Categorias</option>
+    <option v-for="genre of genres" :key="genre.id" :value="genre.id">
+      <a :href="genre.id">{{ genre.name }} - {{ genre.id }}</a>
+    </option>
+  </select>
+  <p v-for="genre of genres" :key="genre.id" :id="genre.id">
     <RouterLink to="/">{{ genre.name }} - {{ genre.id }}</RouterLink>
     <MoviesComp :genre="genre.id" />
   </p>
